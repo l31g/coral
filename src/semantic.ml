b2a3e0dc7c13927910d63f7292e639bd32631d4e
Brian <bhw2113@columbia.edu>
2013-05-09 17:03:28 -0400

    adding symbol table

--- src/semantic.ml
+++ src/semantic.ml
@@ -1,8 +1,32 @@
 open Ast
 
 (* make the symbol table *)
+type symbol_table = {
+	parent : symbol_table option;
+	variables : var_decl list;
+	functions : func_def list;
+	tables : table_label list;
+	in_loop : bool;
+}
 
+(* check types *)
 
+(* check operators *)
+
+(* check functions *)
+
+(* check variables *)
+let rec check_variable v env =
+	try
+		List.find (fun var_decl.(_, vname, _) -> vname = v) env.variables
+	with Not_found ->
+		match env.parent with
+			Some(parent) -> check_variable v parent
+			| _ -> raise (Failure ("Declare your variable bro"))
+
+(* check tables *)
+
+(* check db connection section *)
 let rec check_conn_label co =
     match co with
     | ServerConn -> co
