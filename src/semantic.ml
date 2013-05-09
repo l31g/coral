95c3d47a21e67f81f0108ca40b299f5a6457c39f
mayanez <may2114@columbia.edu>
2013-05-09 15:21:14 -0400

    Awwww yeah lets do this semantic analyzer

--- src/semantic.ml
+++ src/semantic.ml
@@ -1,8 +1,23 @@
 open Ast
 
 (* make the symbol table *)
-type symbol_table = {
-	mutable vars : (t * string) list;
-	mutable funcs : (t * string * (t list)) list;
-	bool_scope : bool
-}
+
+
+let rec check_conn_label co =
+    match co with
+    | ServerConn -> co
+
+let rec check_conn_attr ca  =
+    match ca with
+    | ConnAttr(cl, a) -> try
+                            (check_conn_label cl)
+                        with _ ->
+                            raise (Failure ("You crazy bro"))
+
+let rec check_conn_block cb =
+    match cb with
+    | ConnBlock(a1, a2, a3, a4, a5, a6) -> (check_conn_attr a1)
+
+
+let rec check_program program =
+        (check_conn_block program.conn)
