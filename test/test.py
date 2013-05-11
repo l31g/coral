bf21e61742a2a7b75341db293b561933f37865eb
Shane Chin <sac2171@gmail.com>
2013-05-10 22:15:11 -0400

    Updated test script

--- test/test.py
+++ test/test.py
@@ -1,16 +1,12 @@
-import subprocess, os
+import subprocess, os, filecmp
+
+numTests = 0
+numCorrect = 0
 
 def get_immediate_subdirectories(dir):
     return [name for name in os.listdir(dir)
             if os.path.isdir(os.path.join(dir, name))]
     
-
-current_directory = os.getcwd()
-subdirs = get_immediate_subdirectories(current_directory)
-
-fileList = []
-pathToCoral = '../src/build/coralc'
-
 def compileTests(subdirs):
     for dir in subdirs:    
         for file in os.listdir(dir):
@@ -21,14 +17,32 @@ def compileTests(subdirs):
                 
 def runFiles(subdirs):
     for dir in subdirs:    
-        for files in os.listdir(dir):
-            if files.endswith('.clx'):
-                with open('output', 'w') as outfile:
+        for file in os.listdir(dir):
+            if file.endswith('.clx'):
+                with open(file+'.out', 'w') as outfile:
                     subprocess.call(["python", compileFile], stdout=outfile, stderr=outfile)
                     
+def compare():
+    for dir in subdirs:    
+        for file in os.listdir(dir):
+            if file.endswith('.out'):
+                numTests++                            
+                fileName = file.rsplit['.'][0]
+                if(True === filecmp.cmp(file, fileName+'.exp'))
+                    numCorrect++
+                                
 
-            
-print fileList
+
+current_directory = os.getcwd()
+subdirs = get_immediate_subdirectories(current_directory)
+
+fileList = []
+pathToCoral = '../src/build/coralc'
+compileTests(subdirs)
+runFiles(subdirs)
+compare(subdirs)
+
+print numCorrect + ' ' + numTests                    
             
 
 
