10593e8034cbf6ab224a7e544815aa3898d5f307
Shane Chin <sac2171@gmail.com>
2013-05-12 00:26:41 -0400

    got rid of coral error

--- src/coral.ml
+++ src/coral.ml
@@ -4,13 +4,19 @@ open Semantic
 open Parser
 open Scanner
 
+exception Silent of string
 
 let _ =
 
 
    let source = open_in Sys.argv.(1) in
    let lexbuf = Lexing.from_channel source in
-   let program = Parser.program Scanner.token lexbuf in
+   let program = 
+     try
+       Parser.program Scanner.token lexbuf 
+     with _ ->
+       exit 2
+   in
    let _ = Semantic.check_program(program) in
    let python_source_code = (str_of_program program) in
    let file_name = (String.sub Sys.argv.(1) 0 (String.length Sys.argv.(1) - 3)) in
